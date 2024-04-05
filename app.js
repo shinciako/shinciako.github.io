ba3a27f425a6e300d922ad8c31426d6155ecf2f0
shinciako <100320142+shinciako@users.noreply.github.com>
2024-04-05 21:55:51 +0200

    Add celebrity, form, prize

--- app.js
+++ app.js
@@ -11,15 +11,111 @@ document.addEventListener("DOMContentLoaded", function() {
     }, 2000);
 
     const scrollButton = document.getElementById('scrollButton');
+    const scrollButtonNav = document.getElementById('scrollButtonNav');
     const scrollTarget = document.getElementById('scrollTarget');
 
-    scrollButton.addEventListener('click', () => {
-        const scrollTop = scrollTarget.getBoundingClientRect().top + window.scrollY;
-        const offset = 100; 
+    const scrollButtonStars = document.getElementById('scrollButtonStars');
+    const scrollTargetStars = document.getElementById('scrollTargetStars');
+
+    const scrollButtonForm = document.getElementById('scrollButtonForm');
+    const scrollTargetForm = document.getElementById('scrollTargetForm');
+
+
+    function scrollToTarget(target, offset) {
+        const scrollTop = target.getBoundingClientRect().top + window.scrollY;
         window.scrollTo({
-          top: scrollTop - offset, 
-          behavior: 'smooth'
+            top: scrollTop - offset, 
+            behavior: 'smooth'
         });
-    })
+    }
+    
+    scrollButton.addEventListener('click', function() {
+        scrollToTarget(scrollTarget, 100);
+    });
+
+    scrollButtonNav.addEventListener('click', function() {
+        scrollToTarget(scrollTarget, 100);
+    });
+
+    scrollButtonStars.addEventListener('click', function() {
+        scrollToTarget(scrollTargetStars, 150);
+    });
+
+    scrollButtonForm.addEventListener('click', function() {
+        scrollToTarget(scrollTargetForm, 110);
+    });
+
+
+    
 });
+
+function changeData(index) {
+    const starImage = document.querySelector('#scrollTargetStars .image');
+    const starsContent = document.querySelector('#scrollTargetStars .content');
+    const buttons = document.querySelectorAll('#scrollTargetStars .buttons button');
+
+    switch (index) {
+        case 1:
+            starImage.innerHTML = `
+            <img src="images/probierz.png" alt="Zdjęcie Michała Probierza">
+            `;
+            starsContent.innerHTML = `
+                <p>"Każde uderzenie na polu golfowym to nie tylko ruch kija, ale też oddech dla umysłu i regeneracja dla ciała." - Michał Probierz</p>
+                <ul>
+                    <li>Wiek: 51 lat</li>
+                    <li>Klub golfowy: Business Golf Club</li>
+                    <li>Kluby piłkarskie:
+                        <ul>
+                            <li>Widzew Łódź</li>
+                            <li>Pogoń Szczeciń</li>
+                            <li>Górnik Zabrze</li>
+                        </ul>
+                    </li>
+                </ul>
+            `;
+            break;
+        case 2:
+            starImage.innerHTML = `
+            <img src="images/dudek.png" alt="Zdjęcie Jerzego Dudka" style="height: 300px;">
+            `;
+            starsContent.innerHTML = `
+                <p>"W golfa rywalizujesz nie tylko z innymi graczami, ale także z samym sobą, ze swoimi ograniczeniami i słabościami." - Jerzy Dudek</p>
+                <ul>
+                    <li>Wiek: 51 lat</li>
+                    <li>Klub golfowy: Business Golf Club</li>
+                    <li>Kluby piłkarskie:
+                        <ul>
+                            <li>Real Madryt</li>
+                            <li>Liverpool F.C.</li>
+                            <li>Feyenoord Rotterdam</li>
+                        </ul>
+                    </li>
+                </ul>
+            `;
+            break;
+        case 3:
+            starImage.innerHTML = `
+            <img src="images/bednarek.png" alt="Zdjęcie Filipa Bednarka" style="height: 400px;">
+            `;
+            starsContent.innerHTML = `
+                <p>"Każde uderzenie na polu golfowym to nie tylko ruch kija, ale też oddech dla umysłu i regeneracja dla ciała." - Filip Bednarek</p>
+                <ul>
+                    <li>Wiek: 31 lat</li>
+                    <li>Klub golfowy: Business Golf Club</li>
+                    <li>Kluby piłkarskie:
+                        <ul>
+                            <li>Lech Poznań</li>
+                            <li>sc Heerenveen</li>
+                            <li>De Graafschap</li>
+                        </ul>
+                    </li>
+                </ul>
+            `;
+            break;
+        default:
+            break;
+    }
+    buttons.forEach(button => button.classList.remove('active'));
+    buttons[index - 1].classList.add('active');
+}
 
