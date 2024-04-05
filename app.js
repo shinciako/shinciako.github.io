64b05f27b6a02501e9d7ff3823315ffec9873a2d
shinciako <100320142+shinciako@users.noreply.github.com>
2024-04-05 23:08:10 +0200

    Add reasons

--- app.js
+++ app.js
@@ -49,6 +49,70 @@ document.addEventListener("DOMContentLoaded", function() {
     
 });
 
+const reasonsData = [
+    {
+        subtitle: 'Relaksacja mentalna',
+        text: 'Golf jest często grany w naturalnych, malowniczych otoczeniach, co zapewnia spokojną i uspokajającą atmosferę. To może pomóc piłkarzom w zarządzaniu stresem, lękiem i zmęczeniem psychicznym związanym z uprawianiem sportów w warunkach konkurencyjnych.'
+    },
+    {
+        subtitle: 'Poprawa koncentracji',
+        text: 'Gra w golfa wymaga skupienia i uwagi na każdym uderzeniu. Regularna praktyka może pomóc w poprawie koncentracji, co może mieć pozytywny wpływ na inne obszary życia.'
+    },
+    {
+        subtitle: 'Rozwijanie umiejętności strategicznych',
+        text: 'Golf to gra strategiczna, która wymaga analizy terenu, ustawień, siły i kierunku uderzenia. Poprawa umiejętności strategicznych w grze może pomóc w podejmowaniu lepszych decyzji również poza polem golfowym.'
+    },
+    {
+        subtitle: 'Aktywność na świeżym powietrzu',
+        text: 'Gra w golfa odbywa się na otwartej przestrzeni, co umożliwia kontakt z naturą i aktywność na świeżym powietrzu. To może być korzystne dla zdrowia fizycznego i psychicznego.'
+    },
+    {
+        subtitle: 'Kontakt z naturą',
+        text: 'Pola golfowe są często położone w malowniczych, naturalnych miejscach, co daje graczom możliwość cieszenia się pięknem przyrody i kontaktu z nią podczas gry.'
+    },
+    {
+        subtitle: 'Zmniejszenie stresu',
+        text: 'Gra w golfa może pomóc w zmniejszeniu poziomu stresu poprzez skupienie się na grze i otoczeniu. To może prowadzić do uczucia relaksu i spokoju.'
+    },
+    {
+        subtitle: 'Kreowanie więzi społecznych',
+        text: 'Gra w golfa może być doskonałą okazją do budowania więzi społecznych i nawiązywania nowych znajomości. Wspólne przeżycia na polu golfowym mogą zacieśniać relacje między graczami.'
+    },
+    {
+        subtitle: 'Rozwój umiejętności interpersonalnych',
+        text: 'Interakcja z innymi graczami na polu golfowym może pomóc w rozwoju umiejętności interpersonalnych, takich jak komunikacja, współpraca i negocjacje.'
+    },
+    {
+        subtitle: 'Ruch na świeżym powietrzu',
+        text: 'Gra w golfa angażuje różne partie ciała i wymaga poruszania się po otwartym terenie, co sprzyja aktywności fizycznej i zdrowemu stylowi życia.'
+    },
+    {
+        subtitle: 'Rozwój samodyscypliny',
+        text: 'Regularna praktyka golfa wymaga samodyscypliny i samokontroli. Utrzymywanie wysokiego poziomu koncentracji przez całą rundę może pomóc w rozwijaniu tych umiejętności, które mogą być przydatne również w innych obszarach życia.'
+    },
+    {
+        subtitle: 'Możliwość poznania nowych miejsc',
+        text: 'Gra w golfa to doskonała okazja do poznawania nowych miejsc i odkrywania różnorodnych pól golfowych na całym świecie. To może prowadzić do przygód, nowych doświadczeń i fascynujących podróży.'
+    }
+];
+
+
+function changeReason(buttonText) {
+    const reasonTitle = document.getElementById('reasonTitle');
+    const reasonSubtitle = document.getElementById('reasonSubtitle');
+    const reasonText = document.getElementById('reasonText');
+    
+    const buttons = document.querySelectorAll('#scrollTarget .button-container button');
+    const sortedButtons = Array.from(buttons).sort((a, b) => parseInt(a.textContent) - parseInt(b.textContent));
+    
+    sortedButtons.forEach(button => button.classList.remove('active'));
+    sortedButtons[buttonText].classList.add('active');
+
+    reasonTitle.textContent = `Powód ${buttonText + 1}`;
+    reasonSubtitle.textContent = reasonsData[buttonText].subtitle;
+    reasonText.textContent = reasonsData[buttonText].text;
+}
+
 function changeData(index) {
     const starImage = document.querySelector('#scrollTargetStars .image');
     const starsContent = document.querySelector('#scrollTargetStars .content');
